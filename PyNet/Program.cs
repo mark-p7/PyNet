7e6521d112d9d91dd479dc79563927aa4800c647
Mark De Guzman <markdeguzman1142@gmail.com>
2022-10-01 02:54:19 -0700

    Created Pynet

---
+++ PyNet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using IronPython.Runtime;
+using IronPython.Hosting;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var eng = IronPython.Hosting.Python.CreateEngine();
+        var scope = eng.CreateScope();
+        eng.Execute(@"
+def greetings(name):
+    return 'Hello ' + name.title() + '!'
+    
+", scope);
+        dynamic greetings = scope.GetVariable("greetings");
+        System.Console.WriteLine(greetings("world"));
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+<div style="color: #f9f9f7; border: 5px solid #f9f9f7; padding: 10px;">
+    <h1>PYNET</h1>
+    <p style="font-weight: bold; ">Mark De Guzman (mark-p7)<p>
+    <p>Using IronPython and Pythonnet to create some fun projects on .NET 6.0 and Python 3.10</p>
+</div>
