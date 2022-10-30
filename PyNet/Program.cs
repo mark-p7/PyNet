05a2dac6a15a59c1df882515388e09096502b2cc
Mark De Guzman <markdeguzman1142@gmail.com>
2022-10-30 12:53:18 -0700

    added instructions for installation and added prime search algorithm to program.cs

--- PyNet/Program.cs
+++ PyNet/Program.cs
@@ -16,9 +16,26 @@ class Program
         eng.Execute(@"
 def greetings(name):
     return 'Hello ' + name.title() + '!'
+
+def primeSearch(n):
+    primes = []
+    for i in range(2, n):
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            primes.append(i)
+    return primes
     
 ", scope);
         dynamic greetings = scope.GetVariable("greetings");
         System.Console.WriteLine(greetings("world"));
+
+        dynamic primeSearch = scope.GetVariable("primeSearch");
+        var primes = primeSearch(100);
+        for (int i = 0; i < primes.Count; i++)
+        {
+            System.Console.WriteLine(primes[i]);
+        }
     }
 }
